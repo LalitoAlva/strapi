5028beff9de625abc5f4c01e2a6bc9e078ee393b
Eduardo Alva Ramirez <loloman@PMCPA6STN924.pemex.pmx.com>
2025-09-29 16:55:49 -0600

    Ajuste para AzureAD

---
+++ src/plugins/admin-azure-sso/server/controllers/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const azure = require('./azure');
+
+module.exports = {
+  azure,
+};
---
+++ src/plugins/admin-azure-sso/server/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const bootstrap = require('./bootstrap');
+const routes = require('./routes');
+const controllers = require('./controllers');
+const services = require('./services');
+
+module.exports = {
+  bootstrap,
+  routes,
+  controllers,
+  services,
+};
