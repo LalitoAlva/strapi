4408b2acc5fd01bce0fb88fa87bd209898b25a31
Eduardo Cuauhtemoc[D Alva Ramirez <eduardo.cuauhtemoc.alva@pemex.com>
2025-11-20 12:11:49 -0600

    Eliminar carpeta de plugin azure-oauth que causa conflicto

--- app/src/plugins/strapi-plugin-azure-oauth/server/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log('🔵 Cargando plugin azure-oauth - server/index.js');
-
-module.exports = {
-  register: require('./register'),
-  controllers: require('./controllers'),
-  routes: require('./routes'),
-};
--- app/src/plugins/strapi-plugin-azure-oauth/strapi-server.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-console.log('🔵 Cargando strapi-server.js del plugin azure-oauth');
-
-module.exports = require('./server');
